57acf30eeae0a717b2f3b5b087a64247a2839163
younghk12 <hkyoung503@gmail.com>
2025-09-09 09:34:54 -0700

    l5

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: [
+                {id: 0, size: 's'},
+                {id: 1, size: 'm'},
+                {id: 2, size: 'l'},
+                {id: 3, size: 'xl'},
+            ]
         }
     }
 })
