a19af10c19e3aa1f860f89fa07e0a51e5682a2eb
younghk12 <hkyoung503@gmail.com>
2025-09-09 09:46:23 -0700

    l6

--- main.js
+++ main.js
@@ -11,5 +11,14 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        incrementCart() {
+            this.cart += 1;
+        },
+        decrementCart(){
+            this.cart -= 1;
+        }
+
     }
 })
