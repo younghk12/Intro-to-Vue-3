bb6869af7ab24a87ae19cc51e9afc67d3a922720
younghk12 <hkyoung503@gmail.com>
2025-09-09 13:33:13 -0700

    l10

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -7,7 +7,7 @@ app.component('product-display', {
   },
   template: 
   /*html*/
-  `<div class="product-display">
+  `<div class="product-display" >
     <div class="product-container">
       <div class="product-image">
         <img v-bind:src="image">
@@ -39,6 +39,11 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button 
+          class="button" 
+          v-on:click="subFromCart">
+          Sub from Cart
+        </button>
       </div>
     </div>
   </div>`,
@@ -56,7 +61,10 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+      },
+      subFromCart() {
+          this.$emit('sub-from-cart', this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeFromCart(id) {
+            const index = this.cart.indexOf(id);
+
+            this.cart.splice(index, 1);
+        }
+    }
 })
